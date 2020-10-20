2e585d0f32667bb0da352b07d0f01090d3d99db2
lucas-leoni <lucas.leoni09@gmail.com>
2020-10-19 22:28:04 -0300

    added index.html and assets

---
+++ assets/js/lista.js
@@ -0,0 +1,12 @@
+let alterarPosicao1 = document.querySelectorAll('.frutas')[1];
+alterarPosicao1.textContent = ('Pêra');
+
+let alterarPosicao5 = document.querySelectorAll('.frutas')[5];
+alterarPosicao5.innerHTML = ('<span>Goiaba</span>');
+
+let alterarPosicao0 = document.querySelectorAll('.frutas')[0];
+alterarPosicao0.style.color = ('white');
+alterarPosicao0.style.backgroundColor = ('black');
+
+let removerItem0 = document.querySelectorAll('.frutas')[0];
+removerItem0.remove();
